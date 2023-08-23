ac6034eda6606a11b20e6abd984a895e48f0a4f4
kofiboat22@gmail.com <kofiboat22@gmail.com>
2023-08-23 13:57:31 -0500

    kubernetes-setup

---
+++ k8-workstation.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#set it up as the root user
+
+sudo apt install unzip
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo unzip awscliv2.zip
+sudo ./aws/install
+
+#make sure to install as the ubuntu user (kubectl set-up)
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
+   curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+
+
+
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+
+
+
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+
+#kops set sup
+curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+chmod +x kops
+sudo mv kops /usr/local/bin/kops
+
